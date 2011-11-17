953de57c91e88ba5641def12b17ba66041971add
Alvaro Cabrera <alvaro@outofthebox.mx>
2011-11-17 15:54:55 -0600

    Especificaciones para config()

--- include/funciones.php
+++ include/funciones.php
@@ -1,1 +1,14 @@
+<?php
 
+/* Especificaciones */
+
+# asignación sencilla
+config('clave', 'valor');
+
+# asignación múltiple
+config(array('clave' => 'valor'));
+
+# asignación múltiple (fancy)
+config(function ($t) {
+  $t->clave = 'valor';
+});
