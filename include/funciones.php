b7bbecf3622b94e79ac5e5ff37e2beae9cfb28f6
Alvaro Cabrera <alvaro@outofthebox.mx>
2011-11-17 16:17:13 -0600

    El primer spec funciona, y muy bien

--- include/funciones.php
+++ include/funciones.php
@@ -12,3 +12,30 @@ config(array('clave' => 'valor'));
 config(function ($t) {
   $t->clave = 'valor';
 });
+
+
+/* Contenedor estático de configuraciones */
+
+function config($key = NULL, $val = NULL) {
+  static $bag = array();
+
+  if (func_num_args() === 0) {
+    // Si no hay argumentos devolvemos la configruación
+    return $bag;
+  } elseif (is_string($key)) {
+    if (func_num_args() === 1) {
+      // Si solo hay un argumento devolvemos la opción (getter)
+      return isset($bag[$key]) ? $bag[$key] : NULL;
+    } else {
+      // Si hay dos argumentos asignamos un único valor (setter)
+      $bag[$key] = $val;
+    }
+  }
+
+  // TODO: implementar los specs restantes
+}
+
+
+/* Pruebas */
+
+print_r(config());
